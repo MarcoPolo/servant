d38c00d9335ae99aaf06718fe6d96c60e7bb6424
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-09-25 00:26:50 -0400

    removed old clj files

--- src/clj/servant/repl_macro.clj
+++
@@ -1,8 +0,0 @@
-(ns servant.repl-macro
-  (:require [cemerick.austin.repls :refer (browser-connected-repl-js)])
-)
-
-;; This doesn't work, but I'd like it to
-(defmacro get-repl-endpoint []
-  (browser-connected-repl-js))
-
