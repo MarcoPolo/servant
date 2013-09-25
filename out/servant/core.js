c4a49959960ff2b67f1e5d27796739e27888b6aa
Marco Munizaga <marco.munizaga@ufl.edu>
2013-09-25 14:53:00 -0400

    cleanup files

--- out/servant/core.js
+++
@@ -1,9 +0,0 @@
-goog.provide('servant.core');
-goog.require('cljs.core');
-goog.require('clojure.browser.repl');
-console.log("HELLO THERE!");
-(1 + 2);
-servant.core.test = (function test(name){return console.log([cljs.core.str("woot")].join(''));
-});
-goog.exportSymbol('servant.core.test', servant.core.test);
-console.log(servant.core.test.toString());
